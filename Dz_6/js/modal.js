05daa3925adae71f43beb2578642daa835493eeb
AzamatZz <vip.aza19921992@gmail.com>
2024-10-22 20:05:51 +0600

    Dz_6
    Dz_6

---
+++ Dz_6/js/modal.js
@@ -0,0 +1,40 @@
+const modal = document.querySelector('.modal')
+const btnTiger = document.getElementById('btn-get')
+const closeModalBtn = document.querySelector('.modal_close')
+
+const openModal = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+const closeModal = ()=> {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+}
+btnTiger.onclick = ()=> openModal()
+closeModalBtn.onclick = ()=>{
+    closeModal()
+}
+
+
+modal.onclick = (e) => {
+   if(modal === e.target){
+    closeModal()
+   }   
+}
+
+let switcher = true
+window.onscroll = () => {
+    // console.log(window.scrollY + window.innerHeight);
+    // console.log(document.body.scrollHeight);
+const scrollPosition = window.scrollY + window.innerHeight
+const scrollHeight = document.body.scrollHeight
+ if (scrollPosition >= scrollHeight && switcher === true) {
+    openModal()
+    switcher = false
+ }
+  
+}
+
+setTimeout(() => {
+    openModal()
+}, 1000000)
